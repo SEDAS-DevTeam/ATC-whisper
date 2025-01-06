f1f9826f70094d5753aa7af601d2dfc9aba3c77d
HelloWorld7894 <danpojhan@protonmail.com>
2025-01-06 15:26:43 +0100

    trying BUT-FIT model

--- src/infer/model_test.py
+++ src/infer/model_test.py
@@ -1,8 +1,10 @@
 import gradio as gr
+import torch
+from transformers import pipeline
 
 
 def transcribe(audio):
-    return "TODO: rework later"
+    return whisper_pipeline(audio)["text"]
 
 
 iface = gr.Interface(
@@ -10,5 +12,11 @@ iface = gr.Interface(
     inputs=gr.Audio(sources=['microphone', 'upload'], type='filepath'),
     outputs='text'
 )
+
+
+# runtime
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+whisper_pipeline = pipeline(task="automatic-speech-recognition", model="BUT-FIT/whisper-ATC-czech-full", chunk_length_s=30, device=device)
+whisper_pipeline.model.config.forced_decoder_ids = transcribe.tokenizer.get_decoder_prompt_ids(task="transcribe", language="english")
 
 iface.launch(share=False)
