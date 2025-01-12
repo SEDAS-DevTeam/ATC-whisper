9bc69ed887bc568b10c79615c82c48c223058d5d
HelloWorld7894 <danpojhan@protonmail.com>
2025-01-12 15:25:54 +0100

    added debugging capability to my workspace :)

--- src/model/conversion.py
+++ src/model/conversion.py
@@ -17,6 +17,8 @@ import json
 import base64
 import safetensors.torch
 from transformers import pipeline
+import re
+import whisper
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -308,8 +310,46 @@ def bin_to_pt(fname_inp, fname_out):
 
 
 def pretrained_to_pt(fname_out):
+    def hf_to_whisper_states(text): # renaming some of the layers so it would work
+        text = re.sub('.layers.', '.blocks.', text)
+        text = re.sub('.self_attn.', '.attn.', text)
+        text = re.sub('.q_proj.', '.query.', text)
+        text = re.sub('.k_proj.', '.key.', text)
+        text = re.sub('.v_proj.', '.value.', text)
+        text = re.sub('.out_proj.', '.out.', text)
+        text = re.sub('.fc1.', '.mlp.0.', text)
+        text = re.sub('.fc2.', '.mlp.2.', text)
+        text = re.sub('.fc3.', '.mlp.3.', text)
+        text = re.sub('.fc3.', '.mlp.3.', text)
+        text = re.sub('.encoder_attn.', '.cross_attn.', text)
+        text = re.sub('.cross_attn.ln.', '.cross_attn_ln.', text)
+        text = re.sub('.embed_positions.weight', '.positional_embedding', text)
+        text = re.sub('.embed_tokens.', '.token_embedding.', text)
+        text = re.sub('model.', '', text)
+        text = re.sub('attn.layer_norm.', 'attn_ln.', text)
+        text = re.sub('.final_layer_norm.', '.mlp_ln.', text)
+        text = re.sub('encoder.layer_norm.', 'encoder.ln_post.', text)
+        text = re.sub('decoder.layer_norm.', 'decoder.ln.', text)
+        text = re.sub('proj_out.weight', 'decoder.token_embedding.weight', text)
+        return text
+
     from transformers import WhisperForConditionalGeneration
 
     model = WhisperForConditionalGeneration.from_pretrained("BUT-FIT/whisper-ATC-czech-full")
-    torch.save(model.state_dict(), fname_out)
-    print(f"Model successfully saved.")
+    state_dict = model.state_dict()
+
+    # Rename layers
+    for key in list(state_dict.keys())[:]:
+        new_key = hf_to_whisper_states(key)
+        state_dict[new_key] = state_dict.pop(key)
+
+    # sanity check
+    whisper_model = whisper.load_model("medium")
+    whisper_model.load_state_dict(state_dict)
+
+    print(state_dict)
+
+    print("Converted to whisper")
+
+    torch.save(state_dict, fname_out)
+    print("Model successfully saved")
